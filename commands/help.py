6c1c951cebe08eb1ec2aacc6bd7beeda01b7f840
Okomekomekome12 <okomekomekome1@outlook.com>
2026-07-19 21:54:26 +0900

    modified:   commands/help.py

--- commands/help.py
+++ commands/help.py
@@ -1,7 +1,7 @@
 def help(cw):
     cw.messagesend("""[info][title]これはカレハbotの後継機のさざなみbotのコマンド一覧です[/title]
-[code]/help-sazanami[/code][hr]さざなみのコマンド一覧を出します。ただそれだけ（
-[code]/youtube {youtubeの動画リンク}[/code][hr]youtubeのストリームリンクを出します
+[code]/help-sazanami[/code]さざなみのコマンド一覧を出します。ただそれだけ（[hr]
+[code]/youtube {youtubeの動画リンク}[/code]youtubeのストリームリンクを出します[hr]
 ver 2026.07.10(初期型) さざなみbot -made by 古米[/info]
 """
 )
